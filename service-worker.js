bdf1010ca961c3885bd88f18a12983cb28147731
Daisuke Yukita <dyukita@tk-m-dyukita.ideo.com>
2018-11-16 13:03:41 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/barcodebattler/precache-manifest.dc4bc5f766868518d28a106662b8ebea.js"
+  "/BarcodeBattler/precache-manifest.60665d3fa0beab86ece76975fd9d44a2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/barcodebattler/index.html", {
+workbox.routing.registerNavigationRoute("/BarcodeBattler/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
